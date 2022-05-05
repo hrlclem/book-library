27f9b389871321193759fecf1785ee4908b3e57b
Clement <perso@clementhurel.fr>
2022-05-05 19:40:55 +0900

    dashboard design

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function Book(title, author, pages, read) {
 // Add book to Library
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
-    addBookToLibrary("Yes Man", "Bob Dylan", 43, true);
+    myLibrary.push(newBook);
 }
 
+
+addBookToLibrary("Yes Man", "Bob Dylan", 43, true);
+addBookToLibrary("No Man", "Ron Dylan", 34, false);
+addBookToLibrary("Maybe Man", "Dan Dylan", 73, true);
